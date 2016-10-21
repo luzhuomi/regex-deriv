6378f6f12afac61f3b4a1d89b05c70f8c065df04
Kenny Lu <luzhuomi@gmail.com>
2016-10-21 10:40:19 +0800

    added Universality test

--- Text/Regex/Deriv/Diagnosis/Universality.hs
+++ Text/Regex/Deriv/Diagnosis/Universality.hs
@@ -1,2 +1,37 @@
 module Text.Regex.Deriv.Diagnosis.Universality where
 
+-- Universality test
+-- Let deriv(sigma*)(r) denotes all derivative descandants of r
+-- We say r is universal iff for all t \in deriv(sigma*), () \in t
+
+import Data.List 
+import Data.Char
+import Data.Maybe 
+import qualified Data.Map as M
+
+import Text.Regex.Deriv.RE
+import Text.Regex.Deriv.Common
+import Text.Regex.Deriv.Pretty
+import Text.Regex.Deriv.IntPattern (strip)
+import Text.Regex.Deriv.Parse
+import Text.Regex.Deriv.Diagnosis.Ambiguity (deriv,simp)
+-- import System.IO.Unsafe (unsafePerformIO)
+
+-- ^ compute deriv(sigma*)(r)
+
+allDerivs :: [Char] -> RE -> [RE] 
+allDerivs sigma r = go [] [r] 
+  where go :: [RE] -> [RE] -> [RE]
+        go sofar [] = sofar
+        go sofar rs = 
+          let rs' = nub $ filter (\r -> not (r `elem` sofar)) $ 
+                    (concatMap (\r -> [ simp $ deriv r l | l <- sigma ]) rs)
+          in go (nub (sofar ++ rs)) rs'
+             
+
+univeral :: [Char] -> RE -> Bool 
+univeral sigma r = all posEps (allDerivs sigma r)
+          
+                   
+ascii :: [Char]                    
+ascii = map chr [0..255]
